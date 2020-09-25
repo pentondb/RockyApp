cbf6d88afe2944eff441df9d22c5456a2e97a1a7
Derek Penton <derek.penton@hotmail.com>
2020-09-24 22:10:14 -0500

    Completed upsert functionality

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
@@ -106,39 +107,40 @@ namespace Rocky.Controllers
                 else
                 {
                     // updating
+
+                    // EntityFrameworkCore method AsNoTracking() added here, so that we can update the object from productVM.Product
+                    var objFromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagePath;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var oldFile = Path.Combine(upload, objFromDb.Image);
+
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productVM.Product.Image = fileName + extension;
+                    }
+                    else
+                    {
+                        productVM.Product.Image = objFromDb.Image;
+                    }
+                    _db.Product.Update(productVM.Product);
                 }
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(productVM);
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var obj = _db.Product.Find(id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Product.Update(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
         }
 
         [HttpGet]
--- Rocky/WC.cs
+++ Rocky/WC.cs
@@ -7,6 +7,6 @@ namespace Rocky
 {
     public static class WC
     {
-        public static string ImagePath = @"\images\product";
+        public static string ImagePath = @"\images\product\";
     }
 }
