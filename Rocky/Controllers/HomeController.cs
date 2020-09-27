db49df3b1c9f08e6acc890299a28401bb5a416ef
Derek Penton <derek.penton@hotmail.com>
2020-09-27 17:00:56 -0500

    Added add to cart functionality

--- Rocky/Controllers/HomeController.cs
+++ Rocky/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
+using Rocky.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -43,6 +44,21 @@ namespace Rocky.Controllers
                 ExistsInCart = false
             };
             return View(detailsVM);
+        }
+
+        // see https://www.udemy.com/course/aspnet-core-mvc-up-and-running-part-1-net-5/learn/lecture/21009110#questions
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id});
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
