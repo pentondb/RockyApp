ed4fd4b6ac2234119f05fdfb770e73bdfca6fcdf
Derek Penton <derek.penton@hotmail.com>
2020-09-27 20:50:05 -0500

    Register first user. See https://www.udemy.com/course/aspnet-core-mvc-up-and-running-part-1-net-5/learn/lecture/21009182#questions

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -95,6 +95,7 @@ namespace Rocky.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, WC.AdminRole);
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
